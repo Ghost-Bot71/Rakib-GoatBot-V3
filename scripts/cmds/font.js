f04863dfcee15ade971a6fc77882722a92aeda86
bdrakib6t9 <bdrakib6t9@gmail.com>
2026-06-19 10:11:43 +0600

    Update font.js

--- scripts/cmds/font.js
+++ scripts/cmds/font.js
@@ -1,14 +1,15 @@
+const { loadOwner } = require("../../rakib/customId/ownerUid");
+
 module.exports = {
   config: {
     name: "font",
-    version: "1.0",
+    version: "2.0",
     author: "Rakib",
     countDown: 5,
     role: 0,
     category: "utility",
     guide: {
-      en:
-`font list
+      en: `font list
 font <number> <text>
 
 Example:
@@ -19,32 +20,40 @@ font 2 Rakib`
 
   onStart: async function ({ message, args }) {
 
+    const { ownerUid } = loadOwner();
+
+    if (!ownerUid.includes(message.senderID)) {
+      return message.reply(
+        "❌ | এই কমান্ডটি শুধুমাত্র বটের ওনার ব্যবহার করতে পারবেন।"
+      );
+    }
+
     if (!args[0]) {
       return message.reply(
-        "❌ | Use:\nfont list\nfont <number> <text>"
+`✨ FONT SYSTEM
+
+font list
+font <number> <text>
+
+Example:
+font 2 Rakib Hasan`
       );
     }
 
     const fonts = getFonts();
 
-    /* ================= LIST ================= */
-
     if (args[0].toLowerCase() === "list") {
 
-  let msg = `✨ you'r font list\n\n`;
+      let msg = "✨ AVAILABLE FONTS ✨\n\n";
 
-  fonts.forEach((font, index) => {
+      fonts.forEach((font, index) => {
+        msg += `${index + 1}. ${font.preview}\n`;
+      });
 
-    const styledName = font.convert(font.name);
+      msg += `\n📌 Example:\nfont 2 Rakib`;
 
-    msg += `${index + 1}. ${styledName}\n`;
-
-  });
-
-  return message.reply(msg.trim());
-}
-
-    /* ================= CONVERT ================= */
+      return message.reply(msg);
+    }
 
     const fontNumber = parseInt(args[0]);
 
@@ -54,7 +63,7 @@ font 2 Rakib`
       fontNumber > fonts.length
     ) {
       return message.reply(
-        "❌ | Invalid font number."
+        `❌ Invalid Font Number\n\nUse: font list`
       );
     }
 
@@ -62,264 +71,274 @@ font 2 Rakib`
 
     if (!text) {
       return message.reply(
-        "❌ | Please provide text."
+        "❌ Please provide text."
       );
     }
 
     const selectedFont = fonts[fontNumber - 1];
 
-    const result = selectedFont.convert(text);
-
-    return message.reply(result);
-
+    return message.reply(
+      selectedFont.convert(text)
+    );
   }
 };
 
-/* ================= FONTS ================= */
-
 function getFonts() {
-
   return [
-    
     {
-      name: "sans",
-      convert: serifFont
+      name: "Normal",
+      preview: "Normal Text",
+      convert: normalFont
     },
-
     {
       name: "Bold",
+      preview: "𝐁𝐨𝐥𝐝 𝐓𝐞𝐱𝐭",
       convert: boldFont
     },
-
     {
       name: "Italic",
+      preview: "𝘈𝘭𝘪𝘵𝘢𝘭𝘪𝘤 𝘛𝘦𝘹𝘵",
       convert: italicFont
     },
-
     {
-      name: "Mono",
+      name: "Bold Italic",
+      preview: "𝑩𝒐𝒍𝒅 𝑰𝒕𝒂𝒍𝒊𝒄",
+      convert: boldItalicFont
+    },
+    {
+      name: "Monospace",
+      preview: "𝙼𝚘𝚗𝚘 𝚃𝚎𝚡𝚝",
       convert: monoFont
     },
-
     {
       name: "Script",
+      preview: "𝒮𝒸𝓇𝒾𝓅𝓉 𝒯𝑒𝓍𝓉",
       convert: scriptFont
     },
-
     {
-      name: "Bubble",
-      convert: bubbleFont
+      name: "Bold Script",
+      preview: "𝓑𝓸𝓵𝓭 𝓢𝓬𝓻𝓲𝓹𝓽",
+      convert: boldScriptFont
     },
-
     {
-      name: "Tiny",
-      convert: tinyFont
+      name: "Fraktur",
+      preview: "𝔉𝔯𝔞𝔨𝔱𝔲𝔯",
+      convert: frakturFont
     },
-
     {
-      name: "Fullwidth",
-      convert: fullWidthFont
+      name: "Double Struck",
+      preview: "𝔻𝕠𝕦𝕓𝕝𝕖 𝕋𝕖𝕩𝕥",
+      convert: doubleFont
     },
-
-    {
-      name: "Squared",
-      convert: squaredFont
-    },
-
     {
       name: "Circled",
+      preview: "Ⓒⓘⓡⓒⓛⓔⓓ",
       convert: circledFont
+    },
+    {
+      name: "Squared",
+      preview: "🅂🅀🅄🄰🅁🄴🄳",
+      convert: squaredFont
+    },
+    {
+      name: "Tiny",
+      preview: "ᵀⁱⁿʸ ᵀᵉˣᵗ",
+      convert: tinyFont
+    },
+    {
+      name: "Full Width",
+      preview: "Ｆｕｌｌ Ｗｉｄｔｈ",
+      convert: fullWidthFont
     }
-
   ];
+      }
 
+
+function convertText(text, chars) {
+  return text.replace(/[A-Za-z0-9]/g, char => chars[char] || char);
 }
 
-/* ================= FONT FUNCTIONS ================= */
-function boldFont(text) {
+/* ================= NORMAL ================= */
 
-  const chars = {
-    A:"𝐀",B:"𝐁",C:"𝐂",D:"𝐃",E:"𝐄",
-    F:"𝐅",G:"𝐆",H:"𝐇",I:"𝐈",J:"𝐉",
-    K:"𝐊",L:"𝐋",M:"𝐌",N:"𝐍",O:"𝐎",
-    P:"𝐏",Q:"𝐐",R:"𝐑",S:"𝐒",T:"𝐓",
-    U:"𝐔",V:"𝐕",W:"𝐖",X:"𝐗",Y:"𝐘",
-    Z:"𝐙",
-
-    a:"𝐚",b:"𝐛",c:"𝐜",d:"𝐝",e:"𝐞",
-    f:"𝐟",g:"𝐠",h:"𝐡",i:"𝐢",j:"𝐣",
-    k:"𝐤",l:"𝐥",m:"𝐦",n:"𝐧",o:"𝐨",
-    p:"𝐩",q:"𝐪",r:"𝐫",s:"𝐬",t:"𝐭",
-    u:"𝐮",v:"𝐯",w:"𝐰",x:"𝐱",y:"𝐲",
-    z:"𝐳",
-
-    0:"𝟎",1:"𝟏",2:"𝟐",3:"𝟑",4:"𝟒",
-    5:"𝟓",6:"𝟔",7:"𝟕",8:"𝟖",9:"𝟗"
-  };
-
-  return convertText(text, chars);
-},
-
-
-function boldFont(text) {
-
-  const chars = {
-    A:"𝐀",B:"𝐁",C:"𝐂",D:"𝐃",E:"𝐄",
-    F:"𝐅",G:"𝐆",H:"𝐇",I:"𝐈",J:"𝐉",
-    K:"𝐊",L:"𝐋",M:"𝐌",N:"𝐍",O:"𝐎",
-    P:"𝐏",Q:"𝐐",R:"𝐑",S:"𝐒",T:"𝐓",
-    U:"𝐔",V:"𝐕",W:"𝐖",X:"𝐗",Y:"𝐘",
-    Z:"𝐙",
-
-    a:"𝐚",b:"𝐛",c:"𝐜",d:"𝐝",e:"𝐞",
-    f:"𝐟",g:"𝐠",h:"𝐡",i:"𝐢",j:"𝐣",
-    k:"𝐤",l:"𝐥",m:"𝐦",n:"𝐧",o:"𝐨",
-    p:"𝐩",q:"𝐪",r:"𝐫",s:"𝐬",t:"𝐭",
-    u:"𝐮",v:"𝐯",w:"𝐰",x:"𝐱",y:"𝐲",
-    z:"𝐳",
-
-    0:"𝟎",1:"𝟏",2:"𝟐",3:"𝟑",4:"𝟒",
-    5:"𝟓",6:"𝟔",7:"𝟕",8:"𝟖",9:"𝟗"
-  };
-
-  return convertText(text, chars);
+function normalFont(text) {
+  return text;
 }
+
+/* ================= BOLD ================= */
+
+function boldFont(text) {
+  return text.replace(/[A-Za-z0-9]/g, c => {
+    const code = c.charCodeAt(0);
+
+    if (code >= 65 && code <= 90)
+      return String.fromCodePoint(0x1D400 + (code - 65));
+
+    if (code >= 97 && code <= 122)
+      return String.fromCodePoint(0x1D41A + (code - 97));
+
+    if (code >= 48 && code <= 57)
+      return String.fromCodePoint(0x1D7CE + (code - 48));
+
+    return c;
+  });
+}
+
+/* ================= ITALIC ================= */
 
 function italicFont(text) {
+  return text.replace(/[A-Za-z]/g, c => {
+    const code = c.charCodeAt(0);
 
-  const chars = {
-    A:"𝘈",B:"𝘉",C:"𝘊",D:"𝘋",E:"𝘌",
-    F:"𝘍",G:"𝘎",H:"𝘏",I:"𝘐",J:"𝘑",
-    K:"𝘒",L:"𝘓",M:"𝘔",N:"𝘕",O:"𝘖",
-    P:"𝘗",Q:"𝘘",R:"𝘙",S:"𝘚",T:"𝘛",
-    U:"𝘜",V:"𝘝",W:"𝘞",X:"𝘟",Y:"𝘠",
-    Z:"𝘡",
+    if (code >= 65 && code <= 90)
+      return String.fromCodePoint(0x1D434 + (code - 65));
 
-    a:"𝘢",b:"𝘣",c:"𝘤",d:"𝘥",e:"𝘦",
-    f:"𝘧",g:"𝘨",h:"𝘩",i:"𝘪",j:"𝘫",
-    k:"𝘬",l:"𝘭",m:"𝘮",n:"𝘯",o:"𝘰",
-    p:"𝘱",q:"𝘲",r:"𝘳",s:"𝘴",t:"𝘵",
-    u:"𝘶",v:"𝘷",w:"𝘸",x:"𝘹",y:"𝘺",
-    z:"𝘻"
-  };
+    if (code >= 97 && code <= 122)
+      return String.fromCodePoint(0x1D44E + (code - 97));
 
-  return convertText(text, chars);
+    return c;
+  });
 }
+
+/* ================= BOLD ITALIC ================= */
+
+function boldItalicFont(text) {
+  return text.replace(/[A-Za-z]/g, c => {
+    const code = c.charCodeAt(0);
+
+    if (code >= 65 && code <= 90)
+      return String.fromCodePoint(0x1D468 + (code - 65));
+
+    if (code >= 97 && code <= 122)
+      return String.fromCodePoint(0x1D482 + (code - 97));
+
+    return c;
+  });
+}
+
+/* ================= MONOSPACE ================= */
 
 function monoFont(text) {
+  return text.replace(/[A-Za-z0-9]/g, c => {
+    const code = c.charCodeAt(0);
 
-  const chars = {
-    A:"𝙰",B:"𝙱",C:"𝙲",D:"𝙳",E:"𝙴",
-    F:"𝙵",G:"𝙶",H:"𝙷",I:"𝙸",J:"𝙹",
-    K:"𝙺",L:"𝙻",M:"𝙼",N:"𝙽",O:"𝙾",
-    P:"𝙿",Q:"𝚀",R:"𝚁",S:"𝚂",T:"𝚃",
-    U:"𝚄",V:"𝚅",W:"𝚆",X:"𝚇",Y:"𝚈",
-    Z:"𝚉",
+    if (code >= 65 && code <= 90)
+      return String.fromCodePoint(0x1D670 + (code - 65));
 
-    a:"𝚊",b:"𝚋",c:"𝚌",d:"𝚍",e:"𝚎",
-    f:"𝚏",g:"𝚐",h:"𝚑",i:"𝚒",j:"𝚓",
-    k:"𝚔",l:"𝚕",m:"𝚖",n:"𝚗",o:"𝚘",
-    p:"𝚙",q:"𝚚",r:"𝚛",s:"𝚜",t:"𝚝",
-    u:"𝚞",v:"𝚟",w:"𝚠",x:"𝚡",y:"𝚢",
-    z:"𝚣"
-  };
+    if (code >= 97 && code <= 122)
+      return String.fromCodePoint(0x1D68A + (code - 97));
 
-  return convertText(text, chars);
+    if (code >= 48 && code <= 57)
+      return String.fromCodePoint(0x1D7F6 + (code - 48));
+
+    return c;
+  });
 }
+
+
+/* ================= SCRIPT ================= */
 
 function scriptFont(text) {
-
-  const chars = {
-    A:"𝒜",B:"𝐵",C:"𝒞",D:"𝒟",E:"𝐸",
-    F:"𝐹",G:"𝒢",H:"𝐻",I:"𝐼",J:"𝒥",
-    K:"𝒦",L:"𝐿",M:"𝑀",N:"𝒩",O:"𝒪",
-    P:"𝒫",Q:"𝒬",R:"𝑅",S:"𝒮",T:"𝒯",
-    U:"𝒰",V:"𝒱",W:"𝒲",X:"𝒳",Y:"𝒴",
-    Z:"𝒵",
-
-    a:"𝒶",b:"𝒷",c:"𝒸",d:"𝒹",e:"𝑒",
-    f:"𝒻",g:"𝑔",h:"𝒽",i:"𝒾",j:"𝒿",
-    k:"𝓀",l:"𝓁",m:"𝓂",n:"𝓃",o:"𝑜",
-    p:"𝓅",q:"𝓆",r:"𝓇",s:"𝓈",t:"𝓉",
-    u:"𝓊",v:"𝓋",w:"𝓌",x:"𝓍",y:"𝓎",
-    z:"𝓏"
+  const map = {
+    A:"𝒜",B:"ℬ",C:"𝒞",D:"𝒟",E:"ℰ",F:"ℱ",G:"𝒢",H:"ℋ",I:"ℐ",J:"𝒥",
+    K:"𝒦",L:"ℒ",M:"ℳ",N:"𝒩",O:"𝒪",P:"𝒫",Q:"𝒬",R:"ℛ",S:"𝒮",T:"𝒯",
+    U:"𝒰",V:"𝒱",W:"𝒲",X:"𝒳",Y:"𝒴",Z:"𝒵",
+    a:"𝒶",b:"𝒷",c:"𝒸",d:"𝒹",e:"ℯ",f:"𝒻",g:"ℊ",h:"𝒽",i:"𝒾",j:"𝒿",
+    k:"𝓀",l:"𝓁",m:"𝓂",n:"𝓃",o:"𝑜",p:"𝓅",q:"𝓆",r:"𝓇",s:"𝓈",t:"𝓉",
+    u:"𝓊",v:"𝓋",w:"𝓌",x:"𝓍",y:"𝓎",z:"𝓏"
   };
-
-  return convertText(text, chars);
+  return convertText(text, map);
 }
 
-function bubbleFont(text) {
-  return text.replace(/[A-Za-z]/g, c =>
-    String.fromCodePoint(
-      c <= "Z"
-        ? 0x1F150 + (c.charCodeAt(0) - 65)
-        : 0x1F150 + (c.charCodeAt(0) - 97)
-    )
-  );
+/* ================= BOLD SCRIPT ================= */
+
+function boldScriptFont(text) {
+  return text.replace(/[A-Za-z]/g, c => {
+    const code = c.charCodeAt(0);
+
+    if (code >= 65 && code <= 90)
+      return String.fromCodePoint(0x1D4D0 + (code - 65));
+
+    if (code >= 97 && code <= 122)
+      return String.fromCodePoint(0x1D4EA + (code - 97));
+
+    return c;
+  });
 }
+
+/* ================= FRAKTUR ================= */
+
+function frakturFont(text) {
+  const map = {
+    A:"𝔄",B:"𝔅",C:"ℭ",D:"𝔇",E:"𝔈",F:"𝔉",G:"𝔊",H:"ℌ",I:"ℑ",J:"𝔍",
+    K:"𝔎",L:"𝔏",M:"𝔐",N:"𝔑",O:"𝔒",P:"𝔓",Q:"𝔔",R:"ℜ",S:"𝔖",T:"𝔗",
+    U:"𝔘",V:"𝔙",W:"𝔚",X:"𝔛",Y:"𝔜",Z:"ℨ",
+    a:"𝔞",b:"𝔟",c:"𝔠",d:"𝔡",e:"𝔢",f:"𝔣",g:"𝔤",h:"𝔥",i:"𝔦",j:"𝔧",
+    k:"𝔨",l:"𝔩",m:"𝔪",n:"𝔫",o:"𝔬",p:"𝔭",q:"𝔮",r:"𝔯",s:"𝔰",t:"𝔱",
+    u:"𝔲",v:"𝔳",w:"𝔴",x:"𝔵",y:"𝔶",z:"𝔷"
+  };
+  return convertText(text, map);
+}
+
+/* ================= DOUBLE STRUCK ================= */
+
+function doubleFont(text) {
+  const map = {
+    A:"𝔸",B:"𝔹",C:"ℂ",D:"𝔻",E:"𝔼",F:"𝔽",G:"𝔾",H:"ℍ",I:"𝕀",J:"𝕁",
+    K:"𝕂",L:"𝕃",M:"𝕄",N:"ℕ",O:"𝕆",P:"ℙ",Q:"ℚ",R:"ℝ",S:"𝕊",T:"𝕋",
+    U:"𝕌",V:"𝕍",W:"𝕎",X:"𝕏",Y:"𝕐",Z:"ℤ",
+    a:"𝕒",b:"𝕓",c:"𝕔",d:"𝕕",e:"𝕖",f:"𝕗",g:"𝕘",h:"𝕙",i:"𝕚",j:"𝕛",
+    k:"𝕜",l:"𝕝",m:"𝕞",n:"𝕟",o:"𝕠",p:"𝕡",q:"𝕢",r:"𝕣",s:"𝕤",t:"𝕥",
+    u:"𝕦",v:"𝕧",w:"𝕨",x:"𝕩",y:"𝕪",z:"𝕫",
+    0:"𝟘",1:"𝟙",2:"𝟚",3:"𝟛",4:"𝟜",
+    5:"𝟝",6:"𝟞",7:"𝟟",8:"𝟠",9:"𝟡"
+  };
+  return convertText(text, map);
+}
+
+/* ================= CIRCLED ================= */
+
+function circledFont(text) {
+  const map = {
+    A:"Ⓐ",B:"Ⓑ",C:"Ⓒ",D:"Ⓓ",E:"Ⓔ",F:"Ⓕ",G:"Ⓖ",H:"Ⓗ",I:"Ⓘ",J:"Ⓙ",
+    K:"Ⓚ",L:"Ⓛ",M:"Ⓜ",N:"Ⓝ",O:"Ⓞ",P:"Ⓟ",Q:"Ⓠ",R:"Ⓡ",S:"Ⓢ",T:"Ⓣ",
+    U:"Ⓤ",V:"Ⓥ",W:"Ⓦ",X:"Ⓧ",Y:"Ⓨ",Z:"Ⓩ",
+    a:"ⓐ",b:"ⓑ",c:"ⓒ",d:"ⓓ",e:"ⓔ",f:"ⓕ",g:"ⓖ",h:"ⓗ",i:"ⓘ",j:"ⓙ",
+    k:"ⓚ",l:"ⓛ",m:"ⓜ",n:"ⓝ",o:"ⓞ",p:"ⓟ",q:"ⓠ",r:"ⓡ",s:"ⓢ",t:"ⓣ",
+    u:"ⓤ",v:"ⓥ",w:"ⓦ",x:"ⓧ",y:"ⓨ",z:"ⓩ",
+    0:"⓪",1:"①",2:"②",3:"③",4:"④",
+    5:"⑤",6:"⑥",7:"⑦",8:"⑧",9:"⑨"
+  };
+  return convertText(text, map);
+}
+
+/* ================= SQUARED ================= */
+
+function squaredFont(text) {
+  const map = {
+    A:"🄰",B:"🄱",C:"🄲",D:"🄳",E:"🄴",F:"🄵",G:"🄶",H:"🄷",I:"🄸",J:"🄹",
+    K:"🄺",L:"🄻",M:"🄼",N:"🄽",O:"🄾",P:"🄿",Q:"🅀",R:"🅁",S:"🅂",T:"🅃",
+    U:"🅄",V:"🅅",W:"🅆",X:"🅇",Y:"🅈",Z:"🅉"
+  };
+  return convertText(text.toUpperCase(), map);
+}
+
+/* ================= TINY ================= */
 
 function tinyFont(text) {
-
-  const chars = {
-    a:"ᵃ",b:"ᵇ",c:"ᶜ",d:"ᵈ",e:"ᵉ",
-    f:"ᶠ",g:"ᵍ",h:"ʰ",i:"ᶦ",j:"ʲ",
-    k:"ᵏ",l:"ˡ",m:"ᵐ",n:"ⁿ",o:"ᵒ",
-    p:"ᵖ",q:"ᑫ",r:"ʳ",s:"ˢ",t:"ᵗ",
-    u:"ᵘ",v:"ᵛ",w:"ʷ",x:"ˣ",y:"ʸ",
-    z:"ᶻ"
+  const map = {
+    a:"ᵃ",b:"ᵇ",c:"ᶜ",d:"ᵈ",e:"ᵉ",f:"ᶠ",g:"ᵍ",h:"ʰ",i:"ᶦ",j:"ʲ",
+    k:"ᵏ",l:"ˡ",m:"ᵐ",n:"ⁿ",o:"ᵒ",p:"ᵖ",q:"ᑫ",r:"ʳ",s:"ˢ",t:"ᵗ",
+    u:"ᵘ",v:"ᵛ",w:"ʷ",x:"ˣ",y:"ʸ",z:"ᶻ",
+    0:"⁰",1:"¹",2:"²",3:"³",4:"⁴",
+    5:"⁵",6:"⁶",7:"⁷",8:"⁸",9:"⁹"
   };
-
-  return convertText(text.toLowerCase(), chars);
+  return convertText(text.toLowerCase(), map);
 }
 
-function fullWidthFont(text) {
+/* ================= FULL WIDTH ================= */
 
+function fullWidthFont(text) {
   return text.replace(/[A-Za-z0-9]/g, c =>
     String.fromCharCode(c.charCodeAt(0) + 0xFEE0)
   );
-}
-
-function squaredFont(text) {
-
-  const chars = {
-    A:"🄰",B:"🄱",C:"🄲",D:"🄳",E:"🄴",
-    F:"🄵",G:"🄶",H:"🄷",I:"🄸",J:"🄹",
-    K:"🄺",L:"🄻",M:"🄼",N:"🄽",O:"🄾",
-    P:"🄿",Q:"🅀",R:"🅁",S:"🅂",T:"🅃",
-    U:"🅄",V:"🅅",W:"🅆",X:"🅇",Y:"🅈",
-    Z:"🅉"
-  };
-
-  return convertText(text.toUpperCase(), chars);
-}
-
-function circledFont(text) {
-
-  const chars = {
-    A:"Ⓐ",B:"Ⓑ",C:"Ⓒ",D:"Ⓓ",E:"Ⓔ",
-    F:"Ⓕ",G:"Ⓖ",H:"Ⓗ",I:"Ⓘ",J:"Ⓙ",
-    K:"Ⓚ",L:"Ⓛ",M:"Ⓜ",N:"Ⓝ",O:"Ⓞ",
-    P:"Ⓟ",Q:"Ⓠ",R:"Ⓡ",S:"Ⓢ",T:"Ⓣ",
-    U:"Ⓤ",V:"Ⓥ",W:"Ⓦ",X:"Ⓧ",Y:"Ⓨ",
-    Z:"Ⓩ",
-
-    a:"ⓐ",b:"ⓑ",c:"ⓒ",d:"ⓓ",e:"ⓔ",
-    f:"ⓕ",g:"ⓖ",h:"ⓗ",i:"ⓘ",j:"ⓙ",
-    k:"ⓚ",l:"ⓛ",m:"ⓜ",n:"ⓝ",o:"ⓞ",
-    p:"ⓟ",q:"ⓠ",r:"ⓡ",s:"ⓢ",t:"ⓣ",
-    u:"ⓤ",v:"ⓥ",w:"ⓦ",x:"ⓧ",y:"ⓨ",
-    z:"ⓩ"
-  };
-
-  return convertText(text, chars);
-}
-
-/* ================= COMMON ================= */
-
-function convertText(text, chars) {
-
-  return text.replace(/[A-Za-z0-9]/g, char => {
-    return chars[char] || char;
-  });
-
     }
